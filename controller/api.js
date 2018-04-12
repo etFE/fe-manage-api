74aa48c381af2e11716dc7356307982bf305b72c
Katte <417560450@qq.com>
2018-04-12 15:59:58 +0800

    2018-4-12 katte 组件文档页面获取没有文档的菜单

--- controller/api.js
+++ controller/api.js
@@ -1,11 +1,28 @@
-const { Api } = require('../model');
+const { Api, Menu } = require('../model');
 const errors = require('restify-errors');
 
 // 获取
 const getApis = async (req, res, next) => {
     let result;
     try {
-        result = await Api.find();
+        // result = await Api.find();
+        const result = await Menu.find({ "api": { "$ne": undefined } });
+        // result = menuAndApi.map((menu) => {
+        //     if (menu.api)
+        //         return menu.api;
+        // });
+        res.send({ message: 'success', data: result });
+    } catch (error) {
+        return next(error);
+    }
+    return next();
+}
+
+// 获取没有文档的菜单
+const getEmptyMenu = async (req, res, next) => {
+    let result;
+    try {
+        const result = await Menu.find({ "api": undefined });
         res.send({ message: 'success', data: result });
     } catch (error) {
         return next(error);
@@ -90,7 +107,8 @@ const deleteApiById = async (req, res, next) => {
  */
 exports.get = [
     { path: '/api', system: 'manage', handler: getApis },
-    { path: '/api/:id', system: 'manage', handler: getApiById }
+    { path: '/api/:id', system: 'manage', handler: getApiById },
+    { path: '/api/getEmptyMenu', system: 'manage', handler: getEmptyMenu }
 ];
 
 exports.post = [
